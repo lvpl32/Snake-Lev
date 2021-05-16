f3e3f47ef96527512beb87c44d99c471cf32f1b6
lvpl32 <84021139+lvpl32@users.noreply.github.com>
2021-05-16 21:32:37 +0300

    h

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+
+pygame.init()
+
+win_size = 700
+win = pygame.display.set_mode((win_size,win_size))
+background_color = (255,255,255)
+
+font = pygame.font.SysFont()
+
+def draw_text(text,font,color,frame,x,y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x,y)
+    frame.blit(textobj,textrect)
+
+menu_color = (0,0,0)
+
+def menu(win):
+    running = True
+    event = pygame.event.get()
+    while running:
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        win.fill(menu_color)
+        pygame.display.update()
+        draw_text('Menu', font, red, win, 300,300)
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menu(win)
+
+
+    win.fill(background_color)
+    pygame.display.update()
